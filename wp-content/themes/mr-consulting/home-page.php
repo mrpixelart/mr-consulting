7317ed2407cb561b88cbe32b92237c64d8ede28d
Mitch Ray <mitchray90@gmail.com>
2015-03-28 14:14:03 -0500

    rebuilt homepage with bootstrap grid system, needs additional mobile styling

--- wp-content/themes/mr-consulting/home-page.php
+++ wp-content/themes/mr-consulting/home-page.php
@@ -10,49 +10,53 @@ get_header();
 
 ?>
 
-<div class="ui vertically padded page grid hero-image">
-	<div class="five wide column">
-		<img class="ui image img-bump" src="<?php the_field('hero_image'); ?>">
-	</div>
-	<div class="eleven wide column">
-		<div class="hero">
-			<h1 class="hero-cta"><?php the_field('hero_headline'); ?></h1>
-			<div class="ui divider"></div>
-			<p class="lead"><?php the_field('hero_description'); ?></p>
-			<div class="huge ui inverted teal button "><?php the_field('hero_button'); ?></div>
+<div class="hero-image">
+	<div class="container">
+		<div class="row hero">
+
+			<div class="col-md-5 col-lg-5">
+				<img class="ui image " src="<?php the_field('hero_image'); ?>">
+			</div>
+			<div class="col-md-11 col-lg-11">
+				<h1 class="hero-cta"><?php the_field('hero_headline'); ?></h1>
+				<div class="ui divider"></div>
+				<p class="lead"><?php the_field('hero_description'); ?></p>
+				<div class="huge ui inverted teal button "><?php the_field('hero_button'); ?></div>
+			</div>
 		</div>
 	</div>
 </div>
 
-<div class="ui vertically padded page grid">
+<div class="container">
+	<div class="row section-padding">
+		<div class="col-sm-16">
+			<h2 class="section-header center"><?php the_field('research_headline'); ?></h2>
+		</div>
 
-	<div class="sixteen wide column">
-		<h2 class="section-header center"><?php the_field('research_headline'); ?></h2>
-	</div>
-	<?php if( have_rows('research_section') ): ?>
-		<?php while( have_rows('research_section') ): the_row(); 
+		<?php if( have_rows('research_section') ): ?>
+			<?php while( have_rows('research_section') ): the_row(); 
 
 			// vars
-		$researchHeadline = get_sub_field('section_headline');
-		$researchBody = get_sub_field('section_body');
-		$researchImage = get_sub_field('section_image');
-		$researchCta = get_sub_field('section_cta');
+			$researchHeadline = get_sub_field('section_headline');
+			$researchBody = get_sub_field('section_body');
+			$researchImage = get_sub_field('section_image');
+			$researchCta = get_sub_field('section_cta');
 
-		?>
+			?>
 
-		<div class="eight wide column">
-			<img class="ui image centered" src="<?php echo $researchImage; ?>">
-			<h3><?php echo $researchHeadline; ?></h3>
-			<p><?php echo $researchBody; ?></p>
-			<a class="" href=""><?php echo $researchCta; ?></a>
-		</div>
-	<?php endwhile; ?>
-<?php endif; ?>
-
+			<div class="col-md-8">
+				<img class="ui image centered" src="<?php echo $researchImage; ?>">
+				<h3><?php echo $researchHeadline; ?></h3>
+				<p><?php echo $researchBody; ?></p>
+				<a class="" href=""><?php echo $researchCta; ?></a>
+			</div>
+		<?php endwhile; ?>
+	<?php endif; ?>
+</div>
 </div>
 
-<div class="ui vertically padded page grid hero-image-2">
-	<div class="sixteen wide column">
+<div class="hero-image-2">
+	<div class="container">
 		<div class="hero-2">
 			<h2 class="hero-cta"><?php the_field('collaboration_headline'); ?></h2>
 			<div class="ui divider"></div>
@@ -61,109 +65,117 @@ get_header();
 	</div>
 </div>
 
-<div class="ui page grid">
-	<div class="sixteen wide column">
-		<h2 class="section-header center"><?php the_field('planning_headline'); ?></h2>
-	</div>
-	<?php if( have_rows('planning_section') ): ?>
-		<?php while( have_rows('planning_section') ): the_row(); 
-
-		// vars
-		$documentationHeadline = get_sub_field('documentation_headline');
-		$documentationBody = get_sub_field('documentation_body');
-
-		?>
-
-		<div class="eight wide column">
-			<h3><?php echo $documentationHeadline; ?></h3>
-			<p><?php echo $documentationBody; ?></p>
-		</div>
-	<?php endwhile; ?>
-<?php endif; ?>
-<div class="sixteen wide column center">
-	<a href="#" class="lead"><?php the_field('planning_cta'); ?></a>
-	<img class="ui image" src="<?php the_field('planning_image'); ?>">
-</div>
-</div>
-
-<div class="ui vertically padded page grid hero-image-3">
-	<div class="hero">
-
-		<div class="sixteen wide column">
-			<h2 class="hero-cta center"><?php the_field('build_headline'); ?></h2>
+<div class="container">
+	<div class="row bump-top">
+		<div class="col-sm-16">
+			<h2 class="section-header center"><?php the_field('planning_headline'); ?></h2>
 			<div class="ui divider"></div>
 		</div>
+		<?php if( have_rows('planning_section') ): ?>
+			<?php while( have_rows('planning_section') ): the_row(); 
 
-		<?php if( have_rows('build_section') ): ?>
-			<?php while( have_rows('build_section') ): the_row(); 
-
-			// vars
-			$buildHeadline = get_sub_field('build_headline');
-			$buildBody = get_sub_field('build_body');
-			$buildImage = get_sub_field('build_image');
-			$buildCta = get_sub_field('build_cta');
+		// vars
+			$documentationHeadline = get_sub_field('documentation_headline');
+			$documentationBody = get_sub_field('documentation_body');
 
 			?>
 
-			<div class="ui page grid">
-
-				<div class="four wide column">
-					<img class="ui image" src="<?php echo $buildImage; ?>">
-				</div>
-				<div class="twelve wide column">
-					<h3><?php echo $buildHeadline; ?></h3>
-					<p><?php echo $buildBody; ?></p>
-					<a class="" href=""><?php echo $buildCta; ?></a>
-				</div>
-
-
+			<div class="col-sm-8">
+				<h3><?php echo $documentationHeadline; ?></h3>
+				<p><?php echo $documentationBody; ?></p>
 			</div>
 		<?php endwhile; ?>
 	<?php endif; ?>
+
+	<div class="col-sm-16 center bump-top">
+		<a href="#" class="lead"><?php the_field('planning_cta'); ?></a>
+		<img class="ui image" src="<?php the_field('planning_image'); ?>">
+	</div>
 </div>
 </div>
 
-<div class="ui page grid">
+<div class="hero-image-3">
+	<div class="hero">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-16">
+					<h2 class="hero-cta center"><?php the_field('build_headline'); ?></h2>
+					<div class="ui divider"></div>
+				</div>
+			</div>
+			<?php if( have_rows('build_section') ): ?>
+				<?php while( have_rows('build_section') ): the_row(); 
 
-	<div class="sixteen wide column">
+			// vars
+				$buildHeadline = get_sub_field('build_headline');
+				$buildBody = get_sub_field('build_body');
+				$buildImage = get_sub_field('build_image');
+				$buildCta = get_sub_field('build_cta');
+
+				?>
+
+				<div class="row">
+					<div class="col-md-3 col-md-offset-2">
+						<img class="ui image img-bump" src="<?php echo $buildImage; ?>">
+					</div>
+					<div class="col-md-9">
+						<div class="list-padding">
+							<h3 class="gold"><?php echo $buildHeadline; ?></h3>
+							<p><?php echo $buildBody; ?></p>
+							<a class="" href=""><?php echo $buildCta; ?></a>
+						</div>
+					</div>
+				</div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+</div>
+</div>
+
+
+<div class="container">
+
+	<div class="section-padding">
 		<h2 class="section-header center"><?php the_field('contact_headline'); ?></h2>
 		<div class="ui divider"></div>
-	</div>
-</div>
-<div class="ui page grid">
-	<?php if( have_rows('contact_section') ): ?>
-		<?php while( have_rows('contact_section') ): the_row(); 
+		
+		<div class="row">
+			<?php if( have_rows('contact_section') ): ?>
+				<?php while( have_rows('contact_section') ): the_row(); 
 
 		// vars
-		$connectHeadline = get_sub_field('connect_headline');
-		$connectBody = get_sub_field('connect_body');
+				$connectHeadline = get_sub_field('connect_headline');
+				$connectBody = get_sub_field('connect_body');
 
-		?>
+				?>
 
-		
-		<div class="eight wide column">
-			<h3><?php echo $connectHeadline; ?></h3>
-			<p><?php echo $connectBody; ?></p>
-		</div>
-		
-	<?php endwhile; ?>
-<?php endif; ?>
-</div>
+				
+				<div class="col-md-8">
+					<h3><?php echo $connectHeadline; ?></h3>
+					<p><?php echo $connectBody; ?></p>
+				</div>
 
-<div class="ui page grid">
-	<div class="eight wide column">
-		<p><?php the_content(); ?></p>
+			<?php endwhile; ?>
+		<?php endif; ?>
 	</div>
-	<div class="eight wide column">
-	<div class="ui header"><?php the_field('contact_cta'); ?></div>
-		<div class="ui card">
-			<div class="content">
-				<p><?php the_field('contact_info'); ?></p>
+
+
+	<div class="row">
+		<div class="col-md-8">
+			<p><?php the_content(); ?></p>
+		</div>
+		<div class="col-md-8">
+			<div class="ui header"><?php the_field('contact_cta'); ?></div>
+			<div class="ui card">
+				<div class="content">
+					<p><?php the_field('contact_info'); ?></p>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
+</div>
 
-
+</div> <!-- container -->
 <?php get_footer(); ?>
 
