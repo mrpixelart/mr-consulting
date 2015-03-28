c34169775a4d15be0d12c6fb3dc4387b6ee641bb
Mitch Ray <mitchray90@gmail.com>
2015-03-28 00:55:53 -0500

    first draft of home page art direction and layout

--- wp-content/themes/mr-consulting/home-page.php
+++ wp-content/themes/mr-consulting/home-page.php
@@ -9,170 +9,123 @@
 get_header(); 
 
 ?>
-<script>
-	// $(document).ready(function() {
-	// 	$('#fullpage').fullpage();
-	// });
-</script>
 
-
-<div class="ui vertically padded page grid hero-solid">
-	<div class="column">
+<div class="ui vertically padded page grid hero-image">
+	<div class="five wide column">
+		<img class="ui image img-bump" src="<?php the_field('hero_image'); ?>">
+	</div>
+	<div class="eleven wide column">
 		<div class="hero">
 			<h1 class="hero-cta"><?php the_field('hero_headline'); ?></h1>
+			<div class="ui divider"></div>
 			<p class="lead"><?php the_field('hero_description'); ?></p>
-			<div class="huge ui inverted button "><?php the_field('hero_button'); ?></div>
+			<div class="huge ui inverted teal button "><?php the_field('hero_button'); ?></div>
 		</div>
 	</div>
 </div>
 
-<div class="ui page grid">
+<div class="ui vertically padded page grid">
+
 	<div class="sixteen wide column">
-		<h2 class="ui huge header"><?php the_field('research_headline'); ?></h2>
+		<h2 class="section-header center"><?php the_field('research_headline'); ?></h2>
 	</div>
-	<div class="ui divider"></div>
 	<?php if( have_rows('research_section') ): ?>
 		<?php while( have_rows('research_section') ): the_row(); 
 
 			// vars
-		$headline = get_sub_field('section_headline');
-		$body = get_sub_field('section_body');
+		$researchHeadline = get_sub_field('section_headline');
+		$researchBody = get_sub_field('section_body');
+		$researchImage = get_sub_field('section_image');
+		$researchCta = get_sub_field('section_cta');
 
 		?>
 
 		<div class="eight wide column">
-			<h3><?php echo $headline; ?></h3>
-			<p><?php echo $body; ?></p>
+			<img class="ui image centered" src="<?php echo $researchImage?>">
+			<h3><?php echo $researchHeadline; ?></h3>
+			<p><?php echo $researchBody; ?></p>
+			<a class="" href=""><?php echo $researchCta ?></a>
 		</div>
 	<?php endwhile; ?>
 <?php endif; ?>
 
 </div>
 
-<!-- <div id="fullpage">
-	<div class="section">
-		<nav class="top-nav">
-			<div class="container">
-				<div class="nav-wrapper">
-
-				</div>
-			</div>
-		</nav>
-		<div class="hero-image">
-			<div class="hero-overlay">
-				<div class="container hero">
-					<h1 class="hero-cta"><?php the_field('hero_headline'); ?></h1>
-					<p class="lead "><?php the_field('hero_description'); ?></p>
-
-					<a class=" waves-effect waves-light btn-cta btn-large"><?php the_field('hero_button'); ?></a>
-				</div>
-			</div>
+<div class="ui vertically padded page grid hero-image-2">
+	<div class="sixteen wide column">
+		<div class="hero-2">
+			<h2 class="hero-cta"><?php the_field('collaboration_headline'); ?></h2>
+			<div class="ui divider"></div>
+			<p class="lead"> <?php the_field('collaboration_body'); ?></p>
 		</div>
 	</div>
-	<div class="section">
-		<div class="container hero">
-			<div class="row">
-				<div class="card-panel">
-					<div class="card-content">
-						<h2 class="blue-text callout"><?php the_field('process_headline'); ?></h2>
-					</div>
+</div>
+
+<div class="ui page grid">
+	<div class="sixteen wide column">
+		<h2 class="section-header center"><?php the_field('planning_headline'); ?></h2>
+	</div>
+	<?php if( have_rows('planning_section') ): ?>
+		<?php while( have_rows('planning_section') ): the_row(); 
+
+		// vars
+		$documentationHeadline = get_sub_field('documentation_headline');
+		$documentationBody = get_sub_field('documentation_body');
+
+		?>
+
+		<div class="eight wide column">
+			<h3><?php echo $documentationHeadline; ?></h3>
+			<p><?php echo $documentationBody; ?></p>
+		</div>
+	<?php endwhile; ?>
+<?php endif; ?>
+<div class="sixteen wide column center">
+	<a href="#" class="lead"><?php the_field('planning_cta'); ?></a>
+	<img class="ui image" src="<?php the_field('planning_image'); ?>">
+</div>
+
+</div>
+
+<div class="ui vertically padded page grid hero-image-3">
+	<div class="hero">
+
+		<div class="sixteen wide column">
+			<h2 class="hero-cta center"><?php the_field('build_headline'); ?></h2>
+			<div class="ui divider"></div>
+		</div>
+
+		<?php if( have_rows('build_section') ): ?>
+			<?php while( have_rows('build_section') ): the_row(); 
+
+			// vars
+			$buildHeadline = get_sub_field('build_headline');
+			$buildBody = get_sub_field('build_body');
+			$buildImage = get_sub_field('build_image');
+			$buildCta = get_sub_field('build_cta');
+
+			?>
+
+			<div class="ui page grid">
+
+				<div class="four wide column">
+					<img class="ui image" src="<?php echo $buildImage?>">
+				</div>
+				<div class="twelve wide column">
+					<h3><?php echo $buildHeadline; ?></h3>
+					<p><?php echo $buildBody; ?></p>
+					<a class="" href=""><?php echo $buildCta; ?></a>
 				</div>
 
-				<h4 class="center bump-top"><?php the_field('process_summary'); ?></h4>
-				<?php if( have_rows('process_info') ): ?>
 
-					
-
-					<?php while( have_rows('process_info') ): the_row(); 
-
-						// vars
-					$image = get_sub_field('process_image');
-					$name = get_sub_field('process_name');
-					$description = get_sub_field('process_description');
-
-					?>
-
-					<div class="col s12 m4">
-						<div class="card">
-							<div class="card-image icon-image waves-effect waves-block waves-light">
-								<div class="center">
-									<i class="activator  icon-4x <?php echo $image; ?>"></i>
-								</div>
-							</div>
-							<div class="card-content">
-								<span class="card-title activator grey-text text-darken-4"><?php echo $name; ?> <i class="mdi-navigation-more-vert right"></i></span>
-
-							</div> 
-							<div class="card-reveal">
-								<span class="card-title grey-text text-darken-4"><?php echo $name; ?> <i class="mdi-navigation-close right"></i></span>
-								<p class="card-description"><?php echo $description; ?></p>
-							</div>
-						</div>
-
-					</div>
-
-				<?php endwhile; ?>
-
-
-
-			<?php endif; ?>
-		</div>
-	</div>
-
-	<div class="section">
-		<div class="hero-solid">
-			<div class="container hero ">
-				<h2 class="hero-quote white-text"><?php the_field('hero_quote') ?></h2>
 			</div>
-		</div>
-	</div>
-	<div class="section">
-		<div class="container hero">
-			<div class="row">
-				<div class="col s12 m12">
-					<h2><?php the_field('service_headline'); ?></h2>
-				</div>
-				<?php if( have_rows('pricing_table') ): ?>
-
-					<?php while( have_rows('pricing_table') ): the_row(); 
-
-							// vars
-					$cardTitle = get_sub_field('card_title');
-					$cardPrice = get_sub_field('card_price');
-					$cardDescription = get_sub_field('card_description');
-
-					?>
-					<div class="col s12 m4">
-						<div class="card-panel">
-							<div class="card-content">
-								<h4 class="notop price-title"><?php echo $cardTitle; ?></h4>
-								<span class="card-price"><?php echo $cardPrice; ?></span>
-								<p class="card-description"><?php echo $cardDescription; ?></p>
-							</div>
-						</div>
-					</div>
-				<?php endwhile; ?>
-			<?php endif; ?>
-			<div class="col s12 m12">
-				<h4><?php the_field('customer_service'); ?></h4>
-			</div>
-		</div>
-	</div>
-
-	<div class="section">
-		<div class="hero-solid">
-			<div class="container hero">
-				<div class="card-panel">
-					<div class="card-content">
-						<p><?php the_content(); ?></p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div><!-- #fullpage -->
+		<?php endwhile; ?>
+	<?php endif; ?>
+</div>
+</div>
 
 
+</div>
 
 
 <?php get_footer(); ?>
