60ee58e3510590e1eaa87f1bbcbf2d8894936cab
Mitch Ray <mitchray90@gmail.com>
2015-03-29 15:15:32 -0500

    added strategy section to methodologies

--- wp-content/themes/mr-consulting/my-methodology.php
+++ wp-content/themes/mr-consulting/my-methodology.php
@@ -61,18 +61,18 @@ get_header();
 					<div class="ui divider"></div>
 				</div>
 			</div>
-					<div class="row">
-			<?php if( have_rows('build_section') ): ?>
-				<?php while( have_rows('build_section') ): the_row(); 
+			<div class="row">
+				<?php if( have_rows('build_section') ): ?>
+					<?php while( have_rows('build_section') ): the_row(); 
 
 			// vars
-				$buildHeadline = get_sub_field('build_headline');
-				$buildBody = get_sub_field('build_body');
-				$buildImage = get_sub_field('build_image');
-				$buildCta = get_sub_field('build_cta');
+					$buildHeadline = get_sub_field('build_headline');
+					$buildBody = get_sub_field('build_body');
+					$buildImage = get_sub_field('build_image');
+					$buildCta = get_sub_field('build_cta');
 
-				?>
-		
+					?>
+
 					<div class="col-md-4">
 						<img class="ui image centered" src="<?php echo $buildImage; ?>">
 						<div class="list-padding">
@@ -81,10 +81,48 @@ get_header();
 							<a class="" href=""><?php echo $buildCta; ?></a>
 						</div>
 					</div>
-				
-			<?php endwhile; ?>
-		<?php endif; ?>
+
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
 	</div>
+</div>
+</div>
+
+<div class="container">
+	<div class="row bump-top">
+		<div class="col-sm-16">
+			<h2 class="section-header center"><?php the_field('strategy_title'); ?></h2>
+			<div class="ui divider"></div>
+		</div>
+		<?php if( have_rows('strategy_section') ): ?>
+			<?php while( have_rows('strategy_section') ): the_row(); 
+
+			// vars
+			$strategyHeadline = get_sub_field('strategy_headline');
+			$strategyBody = get_sub_field('strategy_body');
+			$strategyImage = get_sub_field('strategy_image');
+			$strategyCta = get_sub_field('strategy_cta');
+
+			?>
+
+			<div class="col-sm-8">
+				<img class="ui image centered" src="<?php echo $strategyImage; ?>">
+				<h3><?php echo $strategyHeadline; ?></h3>
+				<p><?php echo $strategyBody; ?></p>
+				<a><?php echo $strategyCta; ?></a>
+			</div>
+		<?php endwhile; ?>
+	<?php endif; ?>
+
+	<div class="col-sm-16 bump-top">
+		<div class="ui divider"></div>
+		<h3 class="center"><?php the_field('strategy_feature_headline'); ?></h3>
+		<p class=""><?php the_field('strategy_feature_body'); ?></p>
+		<div class="center">
+			<a href="#" class="lead"><?php the_field('strategy_feature_cta'); ?></a>
+		</div>
+		<img class="ui image centered" src="<?php the_field('strategy_feature_image'); ?>">
 	</div>
 </div>
 </div>
