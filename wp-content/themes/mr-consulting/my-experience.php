e7bf3fc777a4883eb0e9f7e0a26f9d28581c8f8c
Mitch Ray <mitchray90@gmail.com>
2015-03-29 05:53:40 -0500

    set up hero for internal pages, more basic styling

--- wp-content/themes/mr-consulting/my-experience.php
+++ wp-content/themes/mr-consulting/my-experience.php
@@ -9,3 +9,45 @@
 get_header(); 
 
 ?>
+
+<div class="hero-image-2">
+	<div class="container">
+		<div class="row hero">
+			<div class="col-sm-16">
+				<h1 class="hero-cta"><?php the_field('hero_headline'); ?></h1>
+				<div class="ui divider"></div>
+				<p class="lead gold"><?php the_field('hero_description'); ?></p>
+				<!-- <div class="huge ui inverted teal button "><?php the_field('hero_button'); ?></div> -->
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row section-padding">
+		<div class="col-sm-16">
+			<h2 class="section-header center"><?php the_field('research_headline'); ?></h2>
+			<div class="ui divider"></div>
+		</div>
+
+		<?php if( have_rows('research_section') ): ?>
+			<?php while( have_rows('research_section') ): the_row(); 
+
+			// vars
+			$researchHeadline = get_sub_field('section_headline');
+			$researchBody = get_sub_field('section_body');
+			$researchImage = get_sub_field('section_image');
+			$researchCta = get_sub_field('section_cta');
+
+			?>
+
+			<div class="col-md-8">
+				<img class="ui image centered" src="<?php echo $researchImage; ?>">
+				<h3><?php echo $researchHeadline; ?></h3>
+				<p><?php echo $researchBody; ?></p>
+				<a class="" href=""><?php echo $researchCta; ?></a>
+			</div>
+		<?php endwhile; ?>
+	<?php endif; ?>
+</div>
+</div>
--- wp-content/themes/mr-consulting/my-results.php
+++ wp-content/themes/mr-consulting/my-results.php
@@ -9,3 +9,16 @@
 get_header(); 
 
 ?>
+
+<div class="hero-image-3">
+	<div class="container">
+		<div class="row hero">
+			<div class="col-sm-16">
+				<h1 class="hero-cta"><?php the_field('hero_headline'); ?></h1>
+				<div class="ui divider"></div>
+				<p class="lead gold"><?php the_field('hero_description'); ?></p>
+				<!-- <div class="huge ui inverted teal button "><?php the_field('hero_button'); ?></div> -->
+			</div>
+		</div>
+	</div>
+</div>
