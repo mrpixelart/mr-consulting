04b36346ed21c04727f8e7cf9cb19f471917df79
Mitch Ray <mitchray90@gmail.com>
2015-04-03 23:32:24 -0500

    updated favicon link

--- wp-content/themes/mr-consulting/header.php
+++ wp-content/themes/mr-consulting/header.php
@@ -12,7 +12,7 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
 
 	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/bootstrap.css" />
 	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" />
